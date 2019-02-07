621258e94c05bd1d572e44b6f8000ab2a57b9614
Zach Selindh <zachselindh@gmail.com>
2019-02-06 19:14:35 -0600

    Added most visual elements.

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+/* JS file for Train Scheduler web app */
+  
+  
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA4uCzZTJd4E9hBEYL3qa7iG73JXl3pc0s",
+    authDomain: "train-scheduler-9f54a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-9f54a.firebaseio.com",
+    projectId: "train-scheduler-9f54a",
+    storageBucket: "train-scheduler-9f54a.appspot.com",
+    messagingSenderId: "699136325860"
+  };
+  firebase.initializeApp(config);
+
+  
