7e2153d275f7d9631e24bc8d252d1dcb18752093
Zach Selindh <zachselindh@gmail.com>
2019-02-07 23:01:19 -0600

    Added final JS to render correct times and convert vars.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,28 +24,35 @@ $("#submit-button").on("click", function(){
     trainFreq: $("#frequency").val().trim(),
     dateAdded: firebase.database.ServerValue.TIMESTAMP
   };
-  database.ref().push(trainDetails);
-  console.log(trainDetails);
   
+  database.ref().push(trainDetails);
+  $("#train-name").html("");
+  $("#train-destination").html("");
+  $("#train-first-time").html("");
+  $("#frequency").html("");
 });
 
 database.ref().on("child_added", function(snapshot) {
   let nameDisplay = snapshot.val().trainName;
   let destDisplay = snapshot.val().trainDest;
-  let firstTimeDisplay = snapshot.val().trainFirstTime;
-  let freqDisplay = snapshot.val().trainFreq;
+  let frequency = parseInt(snapshot.val().trainFreq);
+  let startTime = moment(snapshot.val().trainFirstTime, "HHmm");
+    startTime = moment(startTime).format("HH:mm");
+      let convertStart = moment(startTime, "HHmm");
+  let timeDifference = moment().diff(moment(convertStart), "minutes");
+  let remaining = timeDifference % frequency;
+  let minutesAway = frequency - remaining;
+  let nextTrain = moment().add(minutesAway, "minutes");
+    let nextTrainDisplay = moment(nextTrain).format("HH:mm");
+
   let newRow = $("<tr><td>" + nameDisplay + 
     "</td><td>" + destDisplay +
-    "</td><td>" + firstTimeDisplay +
-    "</td><td>" + freqDisplay + "</td><td><img src='assets/images/trash-icon.png' class='remove-button'></img></td></tr>");
-  var dataKey = 
+    "</td><td>" + frequency + 
+    "min</td><td>" + nextTrainDisplay +
+    "</td><td>" + minutesAway +
+    "min</td></tr>");
   $("#train-table").append(newRow);
-
-  $(".remove-button").on("click", function(){
-    alert("THIS WORKS");
   });
-});
-
 
 });
 
